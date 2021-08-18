7b286e1945c646138535c7dc627a3ebed5754136
R3mpty <R3mpty@gmail.com>
2021-08-17 22:24:48 -0400

    Adding an AI
    Trying to implement an AI that makes random moves

--- temp.js
+++ temp.js
@@ -11,7 +11,7 @@ Goals:
 // implemented
 const X_CLASS = 'x';
 const O_CLASS = 'o';
-let circleTurn = false;
+let aiTurn = false;
 let winningCombination = [
     [0, 1, 2],
     [3, 4, 5],
@@ -45,44 +45,69 @@ function startGame() {
     }
 
 }
+let currentClass;
 
 function game(e) {
 
     cell = e.target;
-    let currentClass;
 
-    if (circleTurn) {
-        currentClass = O_CLASS;
-    } else {
-        currentClass = X_CLASS;
-    }
 
-    placeMark(cell, currentClass);
-    switchTurn();
+    // if (aiTurn) {
+    //     currentClass = O_CLASS;
+    // } else {
+    //     currentClass = X_CLASS;
+    // }
+
+    // console.log(available(cell))
+    placeMark(cell, X_CLASS);
+    aiDecision(cellElements);
+
 
     if (checkWinner(currentClass)) {
-        console.log(currentClass + " has won! ");
+        if (currentClass == X_CLASS){
+            console.log('The player has won');
+        }
+        else {
+            console.log('The AI has won');
+        }
+        // console.log(currentClass + " has won! ");
         const winningMessage_div = document.querySelector(".winning-message");
         winningMessage_div.style.display = 'flex';
     }
 
 }
 
+function playersTurn(cell){
+    currentClass = X_CLASS;
+    placeMark(cell, X_CLASS);
+}
+
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass);
 }
 
-function switchTurn() {
-    circleTurn = !circleTurn;
-}
+// function switchTurn() {
+//     aiTurn = !aiTurn;
+// }
 
-function filled(cellElements) {
-    for (let i = 0; i < cellElements.length; i++) {
-        if (cellElements === undefined) {
-            return false;
-        }
+// function filled(cellElements) {
+//     for (let i = 0; i < cellElements.length; i++) {
+//         if (cellElements === undefined) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// NOTE: Cell is the one that is currently being targerted, Cell Elements is all the cells available
+        
+function available(cell){
+    if (cell.classList != 'x' || cell.classList != 'o'){
+        return true;
     }
-    return true;
+    else{
+        return false;
+    }
 }
 
 function checkWinner(currentClass) {
@@ -95,12 +120,45 @@ function checkWinner(currentClass) {
 }
 
 function restart() {
-    console.log("hi");
     const winningMessage_div = document.querySelector(".winning-message");
     winningMessage_div.style.display = 'none';
     startGame();
 }
 
 
+function aiDecision(cellElements){
+
+    currentClass = O_CLASS;
+    let madeDecision = false;
+    
+    while (!madeDecision){
+        console.log('This is inside the while loop')
+
+        // Make a random choice between 0 - 8:
+        let choice = getRandomInt(8);
+        let chosenCell = cellElements[choice];
+        console.log(chosenCell);
+
+
+        if (available(chosenCell)){
+            placeMark(chosenCell, O_CLASS);
+            madeDecision = true;
+        }
+
+        else {
+          madeDecision = false  
+        }
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 startGame();
-reset_button.addEventListener("click", restart());
+reset_button.addEventListener("click", restart); // remove paran
+
+/*
+1. How come check winner is working?
+2. How come the availabilty function is not working?
+*/
